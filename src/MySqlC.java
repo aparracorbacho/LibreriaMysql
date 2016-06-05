1017e097bfbb8289dac1cf45b4d2219ea271877d
DogRage <DogRage@192.168.0.13>
2016-06-05 20:27:37 +0200

    Agregamos el metodo para conectar

--- src/MySqlC.java
+++ src/MySqlC.java
@@ -1,6 +1,10 @@
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -20,4 +24,23 @@ public class MySqlC {
     Connection conn = null;
     Statement stmt = null;
     
+ /**
+ * Con el metodo conn definimos la conexion a la base de datos
+ *  @param user El usuario para la conexion
+ *  @param pass El password para la conexion
+ *  @param base El nombre de la base de datos
+ *  @param url La direccion de la base de datos
+ */
+    
+    public void conn(String user, String pass, String base, String url){
+        this.bd = base;
+        this.login = user;
+        this.password = pass;
+        this.url = url;
+        try {
+            conn = (Connection) DriverManager.getConnection(url, login, password);
+        } catch (SQLException ex) {
+            Logger.getLogger(MySqlC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
